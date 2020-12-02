8d93e3161fae0d1baf01883b02337ba08768afa4
Taran van Groenigen <tvangroenigen@outlook.com>
2020-12-02 11:14:35 +0100

    Add react files

--- DotifyV2.Presentation/Startup.cs
+++ DotifyV2.Presentation/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using DotifyV2.Mapping;
 using DotifyV2.Presentation.Middleware.Extensions;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 
 namespace DotifyV2.Presentation
 {
@@ -22,6 +23,11 @@ namespace DotifyV2.Presentation
 		{
 			services.AddApplicationServices();
 			services.AddControllers();
+
+			services.AddSpaStaticFiles(configuration =>
+			{
+				configuration.RootPath = "ClientApp/build";
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,8 +37,14 @@ namespace DotifyV2.Presentation
 			{
 				app.UseDeveloperExceptionPage();
 			}
+			else
+			{
+				app.UseHsts();
+			}
 
 			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseSpaStaticFiles();
 
 			app.UseRouting();
 
@@ -41,6 +53,16 @@ namespace DotifyV2.Presentation
 			app.UseEndpoints(endpoints =>
 			{
 				endpoints.MapControllers();
+			});
+
+			app.UseSpa(spa =>
+			{
+				spa.Options.SourcePath = "ClientApp";
+
+				if (env.IsDevelopment())
+				{
+					spa.UseReactDevelopmentServer(npmScript: "start");
+				}
 			});
 		}
 	}
